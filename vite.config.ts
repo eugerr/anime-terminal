71b6408fbee8e14b0cf0c4bb709064fd65f59e07
eugerr <gardensensei4.com>
2024-06-09 17:56:12 +0800

    added vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,51 +3,52 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// you can copy the base structure of manifest object.
-const manifestForPlugIn = {
-  registerType: 'prompt',
-  includeAssests: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
-  manifest: {
-    name: 'React-vite-app',
-    short_name: 'react-vite-app',
-    description: 'I am a simple vite app',
-    icons: [
-      {
-        src: '/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'favicon',
-      },
-      {
-        src: '/android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'favicon',
-      },
-      {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-        purpose: 'apple touch icon',
-      },
-      {
-        src: '/maskable_icon.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
-      },
-    ],
-    theme_color: '#171717',
-    background_color: '#f0e7db',
-    display: 'standalone',
-    scope: '/',
-    start_url: '/',
-    orientation: 'portrait',
-  },
-}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn)],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'React-vite-app',
+        short_name: 'react-vite-app',
+        description: 'I am a simple vite app',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'favicon',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'favicon',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'apple touch icon',
+          },
+          {
+            src: '/maskable_icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#171717',
+        background_color: '#f0e7db',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
